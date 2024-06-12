11163ed7c1f3f6363f91e154756a39f8df721132
Rushanthan Gunalan <70675057+RushanthanGunalan@users.noreply.github.com>
2024-06-12 21:10:54 +0530

    Front End Setup

---
+++ src/components/Popup.tsx
@@ -0,0 +1,61 @@
+import { ReactNode } from "react";
+import { Button, Modal, Grid, Typography } from "@mui/material";
+
+const Popup = ({
+  open,
+  onClose,
+  title,
+  children,
+}: {
+  open: boolean;
+  onClose: (value: boolean) => void;
+  title: string;
+  children?: ReactNode;
+}) => {
+  return (
+    <Modal
+      open={open}
+      onClose={() => onClose(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Grid
+        direction="column"
+        justifyContent="space-between"
+        alignItems="stretch"
+        sx={{
+          width: 800,
+          backgroundColor: "#fff",
+          boxShadow: "0 0 15px rgba(0, 0, 0, 0.2)",
+          borderRadius: 2,
+        }}
+      >
+        <Grid item>
+          <div
+            style={{
+              padding: 10,
+              paddingLeft: 20,
+              color: "black",
+              textAlign: "left",
+              borderBottom: "1px solid #ccc",
+              transition: "border-bottom 0.3s ease-in-out",
+            }}
+          >
+            <Typography variant="h6">{title}</Typography>
+          </div>
+        </Grid>
+
+        <Grid item style={{ flex: 1, overflowY: "auto", padding: 20 }}>
+          {children}
+        </Grid>
+      </Grid>
+    </Modal>
+  );
+};
+
+export default Popup;
---
+++ src/pages/Contacts.tsx
@@ -0,0 +1,9 @@
+function Contacts() {
+  return (
+    <>
+      <button>Add Contact</button>
+    </>
+  );
+}
+
+export default Contacts;
